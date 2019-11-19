76666a0420afb05199ae9391b3bcfc8691756c5c
Basia <bachna22@gmail.com>
2019-11-19 12:16:39 +0100

    Added Header to App and Router for navigation

--- front-movies/src/App.js
+++ front-movies/src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/header/Header';
 
 export default class App extends Component {
   render() {
-    return <div>TEST</div>;
+    return (
+      <Router>
+        <div>
+          <Header />
+        </div>
+      </Router>
+    );
   }
 }
