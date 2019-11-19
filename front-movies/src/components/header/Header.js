3ddccac77bd60cf1fa7c4831ba02f0f06486d70c
Basia <bachna22@gmail.com>
2019-11-19 12:18:10 +0100

    Created new component Header

---
+++ front-movies/src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect fixed="top" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Navbar.Brand href="home">MovieWeb</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="movies">Movies</Nav.Link>
+            <Nav.Link href="genres">Genres</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="login">Login</Nav.Link>
+            <Nav.Link href="register">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
