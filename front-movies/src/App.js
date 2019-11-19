c641eb50a9ab64ee20f163a700ae101f0441cd64
Basia <bachna22@gmail.com>
2019-11-19 14:21:04 +0100

    Added Header to App.js

--- front-movies/src/App.js
+++ front-movies/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/header/Header';
+import Searchbar from './components/searchbar/Searchbar';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Header />
+        <Header />
+        <div className="content-box">
+          <Searchbar />
         </div>
       </Router>
     );
