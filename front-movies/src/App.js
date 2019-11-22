54280543746c71f32bcaad41aa421429222654b2
Basia <bachna22@gmail.com>
2019-11-22 14:16:02 +0100

    Added route for registration

--- front-movies/src/App.js
+++ front-movies/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Searchbar from './components/searchbar/Searchbar';
 import LoginForm from './components/forms/LoginForm';
+import RegisterForm from './components/forms/RegisterForm';
 
 export default class App extends Component {
   render() {
@@ -13,6 +14,7 @@ export default class App extends Component {
           <Route path="/home" component={Searchbar} />
         </div>
         <Route path="/login" component={LoginForm} />
+        <Route path="/register" component={RegisterForm} />
       </Router>
     );
   }
