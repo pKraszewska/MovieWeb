81b53c1aa9a74bb8e36e224b548f444f1b91a962
Basia <bachna22@gmail.com>
2019-12-08 11:34:40 +0100

    Created api connection that fetches movie data

--- front-movies/src/components/searchbar/Searchbar.js
+++ front-movies/src/components/searchbar/Searchbar.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button, Col, Row } from 'react-bootstrap';
+import SearchContainer from './searchContainer/SearchContainer';
 
 export default class Searchbar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.search = this.search.bind(this);
+
+    this.state = {
+      query: '',
+      result: [],
+      count: 0,
+    };
+  }
+
+  handleInputChange(event) {
+    this.setState({ query: event.target.value });
+  }
+
+  search() {
+    console.log('szukam');
+    fetch(
+      `https://yts.lt/api/v2/list_movies.json?query_term=${this.state.query}`
+    )
+      .then(success => {
+        return success.json();
+      })
+      .then(res => {
+        console.log(res);
+        this.setState({ result: res.data.movies });
+        this.setState({ count: res.data.movie_count });
+        console.log(this.state.result);
+        console.log(this.state.count);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    this.setState({ query: '' });
+  }
+
   render() {
     return (
       <Form>
