e02191c554884b597f21393a97dc9d7c0913701f
Basia <bachna22@gmail.com>
2020-01-13 21:18:44 +0100

    Create paginationBar that commmunicate with BrowseMovies

--- front-movies/src/components/content/BrowseMovies.js
+++ front-movies/src/components/content/BrowseMovies.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MovieContainer from './movieContent/MovieContainer';
 import Searchbar from './searchbar/Searchbar';
+import PaginationBar from './pagination/PaginationBar';
 import './content.css';
 
 export default class BrowseMovies extends Component {
@@ -8,23 +9,23 @@ export default class BrowseMovies extends Component {
     super(props);
     this.getAllMovies = this.getAllMovies.bind(this);
     this.getSearchedMovies = this.getSearchedMovies.bind(this);
+    this.getPage = this.getPage.bind(this);
 
     this.state = {
       movies: [],
       count: 0,
       page_number: 1,
+      limit_per_page: 0,
     };
   }
 
   componentDidMount() {
-    this.getAllMovies();
+    this.getAllMovies(this.state.page_number);
   }
 
-  getAllMovies() {
+  getAllMovies(pageNum) {
     fetch(
-      `https://yts.lt/api/v2/list_movies.json?sort_by=year&&page=${
-        this.state.page_number
-      }`
+      `https://yts.lt/api/v2/list_movies.json?sort_by=year&&page=${pageNum}`
     )
       .then(success => {
         return success.json();
@@ -34,7 +35,8 @@ export default class BrowseMovies extends Component {
         this.setState({ movies: res.data.movies });
         this.setState({ count: res.data.movie_count });
         this.setState({ page_number: res.data.page_number });
-        console.log(this.state.page_number);
+        this.setState({ limit_per_page: res.data.limit });
+        console.log(this.state.limit_per_page);
       })
       .catch(error => {
         console.log(error);
@@ -42,8 +44,15 @@ export default class BrowseMovies extends Component {
   }
 
   getSearchedMovies(data) {
+    console.log(data);
     this.setState({ movies: data.movies });
     this.setState({ count: data.count });
+  }
+
+  getPage(data) {
+    console.log(data);
+    this.setState({ page_number: data });
+    this.getAllMovies(data);
   }
 
   render() {
@@ -51,6 +60,11 @@ export default class BrowseMovies extends Component {
       <React.Fragment>
         <Searchbar sendData={this.getSearchedMovies} />
         <MovieContainer movies={this.state.movies} count={this.state.count} />
+        <PaginationBar
+          sendPage={this.getPage}
+          limit={this.state.limit_per_page}
+          count={this.state.count}
+        />
       </React.Fragment>
     );
   }
