5c23a3e56ca6f7ae26c1bcfee03db483244d08aa
Basia <bachna22@gmail.com>
2020-01-13 11:42:04 +0100

    Changed structure

--- front-movies/src/App.js
+++ front-movies/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header/Header';
-import Searchbar from './components/searchbar/Searchbar';
 import LoginForm from './components/forms/LoginForm';
 import RegisterForm from './components/forms/RegisterForm';
+import BrowseMovies from './components/content/BrowseMovies';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <Header />
-        <Route path="/(home|movies|genres)/" component={Searchbar} />
+        <Route path="/movies" component={BrowseMovies} />
         <Route path="/login" component={LoginForm} />
         <Route path="/register" component={RegisterForm} />
       </Router>
